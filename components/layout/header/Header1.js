cd82ac94a91c66f31cbb1678fd8a76eebf119bb2
ofsevim <mitatcankoc@gmail.com>
2026-01-26 06:03:23 +0300

    add green contact bar to mobile header

--- components/layout/header/Header1.js
+++ components/layout/header/Header1.js
@@ -5,19 +5,6 @@ import Menu from "../Menu"
 export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
     return (
         <>
-            <div className="header-top-bar d-none d-lg-block" style={{ backgroundColor: '#21BB9F', padding: '8px 0', color: '#fff', fontSize: '14px', fontWeight: '500' }}>
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-12 text-center">
-                            <span style={{ marginRight: '10px' }}>Tüm Barkod İhtiyaçlarınız için Hemen Arayabilirsiniz:</span>
-                            <a href="tel:+902163266000" style={{ color: '#fff', textDecoration: 'none', fontWeight: '700' }}>
-                                <i className="fa-solid fa-phone" style={{ marginRight: '5px' }}></i>
-                                0216 326 60 00
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <header id="header" className={`header header-2 header-absolute d-none d-lg-block ${scroll ? "sticky" : ""}`}>
                 <div className="container">
                     <div className="row">
