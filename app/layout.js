22bf07246d660516a28429adab8f0341803b276e
ofsevim <mitatcankoc@gmail.com>
2026-02-11 19:39:25 +0300

    Fix: Remove FA integrity hashes (blocked icons), revert hero to original hero.png

--- app/layout.js
+++ app/layout.js
@@ -29,28 +29,24 @@ export default function RootLayout({ children }) {
             <head>
                 <meta name="google" content="notranslate" />
                 <meta name="google-site-verification" content="UfFYKUqPNXJfJf65qIdWiQ3qXVt2HW5llvBY8Euyn5E" />
-                {/* Font preconnect */}
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
-                {/* FontAwesome 6 Free CDN - solid + brands only (~50KB gzip) yerine lokal 637KB */}
+                {/* FontAwesome 6 Free CDN - solid + brands */}
                 <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/fontawesome.min.css"
-                    integrity="sha512-d0olNN35C6VLiulAobxYHZiXJmq+vl+BGIgAxQtD5+kqudro/xNMvv2yIHAciGHpExsIbKX3iLg+0B/lKrqhQ=="
                     crossOrigin="anonymous"
                     referrerPolicy="no-referrer"
                 />
                 <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/solid.min.css"
-                    integrity="sha512-PnBEJEiVAHnMFpEFOsbrlyDcPJqGFCMEiwByQ/6oKMSdC6Rqed5KhJPKEZ2kqkMaQCRFkGqN7/SJnntRaflbA=="
                     crossOrigin="anonymous"
                     referrerPolicy="no-referrer"
                 />
                 <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css"
-                    integrity="sha512-8RxmFOVaKQe/xtg6lbscU9DU0IRhURWEuiI0tXevv+lXbAHfkpamD4VKFQRto9WgfOJDwOZ74c/s9Yesex3lQ=="
                     crossOrigin="anonymous"
                     referrerPolicy="no-referrer"
                 />
