8da49c2eea67b284cfdea82c39505c06ad060d19
ofsevim <mitatcankoc@gmail.com>
2026-02-11 19:52:50 +0300

    Revert: Back to local fontawesome.css Pro - CDN Free was missing Pro icons

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "@/public/assets/css/plugins/bootstrap.min.css";
+import "@/public/assets/css/plugins/fontawesome.css";
 import "@/public/assets/css/plugins/aos.css";
 import "@/public/assets/css/plugins/slick-slider.css";
 import "@/public/assets/css/plugins/nice-select.css";
@@ -30,26 +31,6 @@ export default function RootLayout({ children }) {
                 <meta name="google" content="notranslate" />
                 <meta name="google-site-verification" content="UfFYKUqPNXJfJf65qIdWiQ3qXVt2HW5llvBY8Euyn5E" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
-                {/* FontAwesome 6 Free CDN - solid + brands */}
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/fontawesome.min.css"
-                    crossOrigin="anonymous"
-                    referrerPolicy="no-referrer"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/solid.min.css"
-                    crossOrigin="anonymous"
-                    referrerPolicy="no-referrer"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css"
-                    crossOrigin="anonymous"
-                    referrerPolicy="no-referrer"
-                />
             </head>
             <body suppressHydrationWarning>{children}</body>
         </html>
